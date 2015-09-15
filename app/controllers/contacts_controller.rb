4ef9917fc0048a259b02dfbf073c9fcae8b5e621
Joel Ibaceta <joel@MacBook-Pro-de-Joel.local>
2015-09-15 04:12:30 -0500

    first release

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,16 @@
 class ContactsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   respond_to :html, :js
   
   def index
-    @contacts = Contact.order(:last_name)
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    @contacts = Contact.order(:last_name).filter_by_user(current_user)
+    @query = "#{params[:query]}%"
+    @contacts = @contacts.where("first_name LIKE ? OR last_name LIKE ? OR address LIKE ? OR phone LIKE ? OR email LIKE ?", 
+                                 @query, @query, @query, @query, @query) if params[:query] != nil
+    
+   
   end
 
   def new
@@ -16,7 +20,7 @@ class ContactsController < ApplicationController
   def create
     contact = Contact.new(contact_params)
     if contact.save
-      @contacts = Contact.order(:last_name)
+      @contacts = Contact.order(:last_name).filter_by_user(current_user)
       
     else
       @contact = contact
@@ -33,15 +37,22 @@ class ContactsController < ApplicationController
   end
   
   def destroy
-    @contact = Contact.find(params[:id])
+    contact = Contact.find(params[:id])
+    contact.destroy!
+    @contacts = Contact.order(:last_name).filter_by_user(current_user)
   end
   
   def update
-    @contact = Contact.find(params[:id])
+    contact = Contact.find(params[:id])
+    if contact.update_attributes(contact_params)
+      @contacts = Contact.order(:last_name).filter_by_user(current_user)
+    else
+      @contact = contact
+    end
   end
   
   private
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :avatar, :address, :phone, :email)
+      params.require(:contact).permit(:first_name, :last_name, :avatar, :address, :phone, :email, :user_id)
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,15 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
   
-  validates :email, uniqueness: true
+  validates_uniqueness_of :email, :scope => :user_id
   validates :first_name, :last_name, :email, :address, presence: true
   
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":id_identicon.png"
      validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   
   after_save :create_identicon 
+  
+  scope :filter_by_user, -> (user) {where("user_id = ?", user.id)}
   
   def name
     "#{self.first_name} #{self.last_name}"
