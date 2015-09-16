5b5fe600217f65a9dae2e7db74aca7fea23dea9c
Joel Ibaceta <joel@MacBook-Pro-de-Joel.local>
2015-09-15 22:09:40 -0500

    logger remote added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.logger = RemoteSyslogLogger.new('logs3.papertrailapp.com', 27175)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
   
-  config.logger = RemoteSyslogLogger.new('logs3.papertrailapp.com', 27175)
+  
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
