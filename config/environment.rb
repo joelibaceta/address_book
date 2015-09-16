c464fd84081df346b3d93d73457064b762ade086
Joel Ibaceta <joel@MacBook-Pro-de-Joel.local>
2015-09-15 22:23:04 -0500

    secret key added for production

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.logger = RemoteSyslogLogger.new('logs3.papertrailapp.com', 27175)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  
+
+  config.logger = RemoteSyslogLogger.new('logs3.papertrailapp.com', 27175)
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
